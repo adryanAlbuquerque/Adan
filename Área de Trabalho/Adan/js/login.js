f0f75428e15d84112601009c5da81a134e1f4737
adryanAlbuquerque <adryanfelipe17@hotmail.com>
2023-06-15 00:02:47 -0300

    adan

--- Área de Trabalho/Adan/js/login.js
+++ Área de Trabalho/Adan/js/login.js
@@ -2,7 +2,14 @@ const input = document.querySelector('.login__input');
 const button = document.querySelector('.neon_button');
 const form = document.querySelector('.login-form');
 
+const validateInput = ({ target }) => {
+  if (target.value.length > 3) {
+    button.removeAttribute('disabled');
+    return;
+  }
 
+  button.setAttribute('disabled', '');
+}
 
 const handleSubmit = (event) => {
   event.preventDefault();
